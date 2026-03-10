a0e4a21af1bd85c621d57ef78f13315d19e781a8
Vrushik Visavadiya <visavadiyavrushik@gmail.com>
2026-03-10 19:12:17 +0530

    feat: initial commit

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,30 @@
+// src/lib/mongodb.ts
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string; // from .env.local
+const options = {};
+
+if (!uri) {
+  throw new Error("Please define MONGODB_URI in .env.local");
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
